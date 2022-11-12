64a278a7ac32ab2fafbf5090f07a736bbd8ed137
Anton <dedulevitchanton@gmail.com>
2022-11-13 00:09:39 +0900

    Добавлен код

---
+++ Program/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символам.
+Console.Clear();
+string[]array = {"Лиса", "\t", "Ёж","\t", "Волк","\t", "Бык","\t", ":)"};
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i]);
+}
+Console.WriteLine();
+Console.WriteLine($"Результат ");
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3) Console.Write(array[i]);
+}
